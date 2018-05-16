feacf55a78a46d4dbb5eb4708172e2e306211d9a
Holden Altaffer <haltaffer@gmail.com>
2018-05-16 23:12:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,9 +49,5 @@ class Dog
 
   def self.new_from_db(row)
     self.new(id: row[0], name: row[1], breed: row[2])
-    # dog.id = row[0]
-    # dog.name = row[1]
-    # dog.breed = row[2]
-    
   end
 end
